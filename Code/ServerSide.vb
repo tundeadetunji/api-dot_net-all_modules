cfbc682598af5b0b671632f80a1bd2496a523aee
Tunde Adetunji <tundeadetunji2017@gmail.com>
2023-07-09 18:16:11 +0100

    Add HTTP functions

---
+++ Code/ServerSide.vb
@@ -0,0 +1,33 @@
+﻿Imports System.Net.Http
+Imports System.Collections.Specialized
+Imports System.Text
+Imports System.Net
+
+Public Class ServerSide
+    Private Function Receive(URL As String)
+        Dim response
+        Using wb = New WebClient()
+            response = wb.DownloadString(URL)
+        End Using
+        Return response
+    End Function
+
+    Private Function DictionaryToNameValueCollection(dictionary As Dictionary(Of String, String))
+        Return dictionary.Aggregate(New NameValueCollection(), Function(seed, current)
+                                                                   seed.Add(current.Key, current.Value)
+                                                                   Return seed
+                                                               End Function)
+
+    End Function
+
+    Private Function Send(URL As String, FormValues As Dictionary(Of String, String))
+        Dim data, response, responseInString
+        Using wb = New WebClient()
+            data = DictionaryToNameValueCollection(FormValues)
+            response = wb.UploadValues(URL, "POST", data)
+            responseInString = Encoding.UTF8.GetString(response)
+        End Using
+        Return responseInString
+    End Function
+
+End Class
