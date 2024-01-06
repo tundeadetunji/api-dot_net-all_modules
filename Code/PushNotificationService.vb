bbc7dd5b3d13e589fd725a1a1e1115607e0ca6ed
Tunde Adetunji <tundeadetunji2017@gmail.com>
2024-01-06 23:50:21 +0100

    Release 3.5.7

---
+++ Code/PushNotificationService.vb
@@ -0,0 +1,56 @@
+﻿Imports FirebaseAdmin
+Imports FirebaseAdmin.Messaging
+Imports Google.Apis.Auth.OAuth2
+Imports iNovation.Code.General
+Public Class PushNotificationService
+
+    Private ReadOnly Property app
+
+    'Private ReadOnly Property firebase_admin_sdk_json_file As String
+    'Private ReadOnly Property credential
+
+    Private ReadOnly Property firebase_token As String
+    ''' <summary>
+    ''' Required to send push notification to Android device.
+    ''' 
+    ''' Ṣe ètò àti ṣe ìfiránṣẹ́ sí ẹ̀rọ ìbánisọ̀rọ̀.
+    ''' </summary>
+    ''' <param name="firebase_admin_sdk_json_file"></param>
+    ''' <param name="firebase_device_token"></param>
+    Public Sub New(firebase_admin_sdk_json_file As String, firebase_device_token As String)
+        Me.firebase_token = firebase_device_token
+        app = FirebaseApp.Create(New AppOptions() With {.Credential = GoogleCredential.FromFile(firebase_admin_sdk_json_file)})
+    End Sub
+
+    Public Sub Push(title As String, message As String)
+
+        'Dim messaging = FirebaseMessaging.GetMessaging(app)
+
+
+        'Dim message = New MulticastMessage() With {
+        '    .Tokens = New List(Of String) From {
+        '        ReadText(firebase_token_file)
+        '    },
+        '    .Notification = New Notification() With {
+        '        .Title = "Hello",
+        '        .Body = "This is a test notification"
+        '    }
+        '}
+        'FirebaseMessaging.GetMessaging(app).SendMulticastAsync(Message)
+        FirebaseMessaging.GetMessaging(app).SendMulticastAsync(New MulticastMessage() With {
+            .Tokens = New List(Of String) From {
+                firebase_token
+            },
+            .Notification = New Notification() With {
+                .Title = title,
+                .Body = message
+            }
+        })
+
+    End Sub
+
+    ''Dim credential = GoogleCredential.FromFile(firebase_admin_sdk_json_file)
+
+
+
+End Class
--- Code/ServerSide.vb
+++ Code/ServerSide.vb
@@ -4,6 +4,7 @@ Imports System.Text
 Imports System.Net
 Imports System.IO
 Imports Newtonsoft.Json
+Imports FluentFTP
 
 Public Class ServerSide
 
@@ -118,7 +119,6 @@ Public Class ServerSide
 
 #End Region
 
-
 #Region "Support"
 
 
@@ -138,4 +138,94 @@ Public Class ServerSide
         Public statusCode As String
     End Structure
 #End Region
+
+
+#Region "Ftp"
+    Class Ftp
+        Private Property server As String
+        Private Property username As String
+        Private Property password As String
+
+        Public Sub New(ByVal ftp_server As String, ByVal username As String, ByVal password As String)
+            Me.server = ftp_server
+            Me.username = username
+            Me.password = password
+        End Sub
+        ''' <summary>
+        ''' Use this if you want to call downloadFile()
+        ''' </summary>
+        Public Sub New()
+
+        End Sub
+
+        Private Function CreateFtpClient() As FtpClient
+            Return New FtpClient(Me.server, New NetworkCredential With {
+            .UserName = username,
+            .Password = password
+        })
+        End Function
+
+        ''' <summary>
+        ''' Uploads content of file.
+        ''' </summary>
+        ''' <param name="filename_with_extension"></param>
+        ''' <param name="fileBytes"></param>
+        ''' <param name="remoteFolderPathEscapedString"></param>
+        ''' <example>
+        ''' new Uploader(...).uploadBytes("1.jpg", PictureFromStream(...), "site.com\wwwroot\images\") //from regular Desktop application
+        ''' new Uploader(...).uploadBytes("1.jpg", bannerPic.FileBytes, "site.com\wwwroot\images\") //from ASP.NET
+        ''' </example>
+        Public Sub uploadBytes(ByVal filename_with_extension As String, ByVal fileBytes As Byte(), ByVal Optional remoteFolderPathEscapedString As String = "") ''As FtpStatus
+            Using ftp As FtpClient = CreateFtpClient()
+                ftp.UploadBytes(fileBytes, remoteFolderPathEscapedString & filename_with_extension)
+            End Using
+        End Sub
+
+        ''' <summary>
+        ''' Called typically from desktop application (from OpenFileDialog thereabouts). 
+        ''' </summary>
+        ''' <param name="info"></param>
+        ''' <param name="remoteFolderPathEscapedString"></param>
+        Public Sub uploadFile(ByVal info As FileInfo, ByVal Optional remoteFolderPathEscapedString As String = "")
+            Using ftp As FtpClient = CreateFtpClient()
+
+                Using stream As FileStream = File.OpenRead(info.FullName)
+                    ftp.UploadStream(stream, remoteFolderPathEscapedString & info.Name)
+                End Using
+            End Using
+        End Sub
+
+        Public Sub uploadFile(ByVal file_to_upload As String, ByVal Optional remoteFolderPathEscapedString As String = "", ByVal Optional new_file_name_with_extension As String = Nothing)
+            Dim filename As String = If(new_file_name_with_extension IsNot Nothing, new_file_name_with_extension, Path.GetFileName(file_to_upload))
+            Dim text() As Byte = File.ReadAllBytes(file_to_upload)
+            uploadBytes(filename, text, remoteFolderPathEscapedString)
+        End Sub
+
+        ''' <summary>
+        ''' Downloads file from remote server.
+        ''' </summary>
+        ''' <param name="remote_file_path"></param>
+        ''' <param name="full_path_of_filename_with_extension_to_store_it_on_local_file_system"></param>
+        ''' <param name="attempt_to_read_package_as_text_on_receive"></param>
+        ''' <returns>the content of the file (assuming it's text) if attempt_to_read_package_as_text_on_receive = true, otherwise full_path_of_filename_with_extension_to_store_it_on_local_file_system</returns>
+        Public Function downloadFile(remote_file_path As String, full_path_of_filename_with_extension_to_store_it_on_local_file_system As String, Optional attempt_to_read_package_as_text_on_receive As Boolean = False) As String
+
+            Dim webClient As New WebClient()
+            webClient.DownloadFile(remote_file_path, full_path_of_filename_with_extension_to_store_it_on_local_file_system)
+
+            Return If(attempt_to_read_package_as_text_on_receive, General.ReadText(full_path_of_filename_with_extension_to_store_it_on_local_file_system), full_path_of_filename_with_extension_to_store_it_on_local_file_system)
+
+            'Using ftp As FtpClient = CreateFtpClient()
+            '    ftp.Connect()
+            '    Try
+            '        ftp.DownloadFile(full_path_of_filename_with_extension_to_store_it_on_local_file_system, remote_file_path)
+            '    Catch ex As Exception
+            '    End Try
+            'End Using
+
+        End Function
+    End Class
+
+#End Region
+
 End Class
