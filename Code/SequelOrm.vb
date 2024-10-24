06b0d9e5c617624337cb91ec0f247b77cb9ca1f1
Tunde Adetunji <tundeadetunji2017@gmail.com>
2024-10-24 22:40:54 +0100

    Finalize Documentation

--- Code/SequelOrm.vb
+++ Code/SequelOrm.vb
@@ -7,7 +7,7 @@ Imports System.Text
 Imports Microsoft.VisualBasic.Devices
 
 ''' <summary>
-''' Lightweight ORM.
+''' Lightweight ORM. Joins are coming in subsequent versions.
 ''' <br />
 ''' Picks up only non read-only, non Collection-type properties, may throw exception if any property is read-only or is IEnumerable/Collection/List/Enum.
 ''' The methods in this class are not asynchronous, do not employ locking mechanisms, and not covering transactions.
