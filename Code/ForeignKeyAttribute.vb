adbcd4ff5d07fab8c8df0ed810a73897e47734f3
Tunde Adetunji <tundeadetunji2017@gmail.com>
2025-06-07 01:31:26 +0100

    Create Checkpoint as start of full upgrade

---
+++ Code/ForeignKeyAttribute.vb
@@ -0,0 +1,10 @@
+﻿<AttributeUsage(AttributeTargets.Property)>
+Public Class ForeignKeyAttribute
+    Inherits Attribute
+
+    Public ReadOnly Property ColumnName As String
+
+    Public Sub New(columnName As String)
+        Me.ColumnName = columnName
+    End Sub
+End Class
---
+++ Code/JoinTableAttribute.vb
@@ -0,0 +1,14 @@
+﻿<AttributeUsage(AttributeTargets.Property)>
+Public Class JoinTableAttribute
+    Inherits Attribute
+
+    Public ReadOnly Property TableName As String
+    Public ReadOnly Property ThisKey As String
+    Public ReadOnly Property OtherKey As String
+
+    Public Sub New(tableName As String, thisKey As String, otherKey As String)
+        Me.TableName = tableName
+        Me.ThisKey = thisKey
+        Me.OtherKey = otherKey
+    End Sub
+End Class
